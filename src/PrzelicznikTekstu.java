71368fda8cfc85f06996f1b31ce2a23ce71ade54
Agnieszka Łozowska <aga.lozowska@wp.pl>
2018-08-26 19:43:49 +0200

    Do poprawienia

---
+++ src/PrzelicznikTekstu.java
@@ -0,0 +1,39 @@
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class PrzelicznikTekstu {
+
+    public static void pokazInformacje(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        PrintWriter printWriter = response.getWriter();
+        String tekst = request.getParameter("tekst");
+
+        obliczZnaki(printWriter, tekst);
+        obliczBezSpacji(printWriter, tekst);
+        obliczWyrazy(printWriter,tekst);
+        sprawdzPalindrom(printWriter, tekst);
+
+    }
+
+
+    public static void obliczZnaki(PrintWriter writer, String tekst){
+        writer.println("ilosc wszystkich znakow: " + tekst.length());
+    }
+    public static void obliczBezSpacji(PrintWriter writer, String tekst){
+        String bezSpacji=tekst.replace(" ", "");
+        writer.println("ilosc znakow bez spacji: " + bezSpacji.length());
+    }
+    public static void obliczWyrazy(PrintWriter writer, String tekst){
+        String []wyrazy=tekst.split(" ");
+        writer.println("ilosc wyrazow: " + wyrazy.length);
+    }
+    public static void sprawdzPalindrom(PrintWriter writer, String tekst) {
+        StringBuilder builder=new StringBuilder();
+        String reversed=builder.reverse().toString();
+        if(reversed.equals(tekst)){
+            writer.println("tekst jest palindromem");
+        }else
+            writer.println("tekst nie jest palindromem");
+    }
+}
---
+++ src/Servlet.java
@@ -0,0 +1,21 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/Servlet")
+public class Servlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        PrzelicznikTekstu.pokazInformacje(request, response);
+
+    }
+
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws  IOException {
+PrzelicznikTekstu.pokazInformacje(request, response);
+    }
+
+
+}
